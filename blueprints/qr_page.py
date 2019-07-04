8d5313977b2ee9d1308f79f4d7cfe02707fa00cf
Rowan Smith <rowanpsmith@yahoo.com.au>
2019-07-05 00:21:13 +1000

    Add QR Code web generator

--- blueprints/qr_page.py
+++ blueprints/qr_page.py
@@ -4,5 +4,8 @@ qr_blueprint = Blueprint('qr_page', __name__)
 
 
 @qr_blueprint.route('/qr')
-def qr_page():
-    return render_template('qrtest.html')
+@qr_blueprint.route('/qr/<url>')
+@qr_blueprint.route('/qr/<url>/<colour>')
+@qr_blueprint.route('/qr/<url>/<colour>/<size>')
+def qr_page(url: str = "it.jcu.io", colour: str = "white", size: int = 60):
+    return render_template('qrtest.html', url=url, colour=colour, size=size)
