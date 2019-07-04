59d8183894e1b8ff16ce899bbce53e940fa9be85
Rowan Smith <rowanpsmith@yahoo.com.au>
2019-07-04 14:50:13 +1000

    Added basic app information.

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index_page():
+    return 'This is the page people first see.'
+
+
+@app.route('/home')
+def home_page():
+    return 'This is the home page for users that are logged in.'
+
+
+@app.route('/login')
+def login_page():
+    return 'This is the login page.'
 
 
 if __name__ == '__main__':
