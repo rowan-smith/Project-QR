d0c49c9fb9269c8f4b0c6018bdaeec6dab532624
Rowan Smith <rowanpsmith@yahoo.com.au>
2019-07-04 17:22:33 +1000

    Updated pages with basic functionality

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, session
+import os
+
+from flask import Flask, render_template, redirect, url_for, session, request, flash
 
 app = Flask(__name__)
 
@@ -11,8 +13,7 @@ def index_page():
 @app.route('/home')
 def home_page():
 
-    # Check is flask session is active
-    if session is None:
+    if session.get('logged_in') is None:
         return redirect(url_for('login_page'))
 
     return render_template('home.html')
@@ -20,8 +21,20 @@ def home_page():
 
 @app.route('/login')
 def login_page():
-    return render_template('login_page.html')
+    x = None
+    if x:
+        session['logged_in'] = True
+        return render_template('home.html')
+    else:
+        flash('wrong password!')
+        return render_template('login_page.html')
+
+
+@app.route('/register')
+def register_page():
+    return render_template('register_page.html')
 
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run()
