95cf9a95c11878416af286ce8712d2febb37a9aa
Rowan Smith <rowanpsmith@yahoo.com.au>
2019-07-05 17:46:09 +1000

    Add points and rename id to uuid.

--- classes/User.py
+++ classes/User.py
@@ -7,11 +7,15 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(db.String(36), unique=True, primary_key=True)
+    # User admin information
+    uuid = db.Column(db.String(36), unique=True, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+
+    # User game information
+    points = db.Column(db.Integer)
 
     @staticmethod
     def set_password(password):
