89461d5ff2fd224fc2514b8cdf2e111e97db659e
Rowan Smith <rowanpsmith@yahoo.com.au>
2019-08-05 17:48:21 +1000

    Update locations links

--- blueprints/locations.py
+++ blueprints/locations.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, abort
+from flask import Blueprint, render_template, session, abort, url_for
 
 from models.ScanLocation import ScanLocation
 
@@ -10,6 +10,10 @@ SCAN_URL = "http://127.0.0.1/scan?location="
 
 @locations_blueprint.route('/locations')
 def locations():
+
+    # return url_for('home_page.home', my_var='my_value', test='test')
+
+    # return url_for('qr_generator_page.qr_code', url=url_for('qr_code_page.qr_scan', location=i.uuid))
 
     if 'username' in session and session['is_admin']:
         return render_template('locations.html', locations=ScanLocation.query.all(), scan_url=SCAN_URL, qr_url=QR_URL)
