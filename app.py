bb5caf5ffe1a754ea9b9c136d88a899f71e9ac87
Rowan Smith <rowanpsmith@yahoo.com.au>
2019-08-07 19:31:47 +1000

    Fixed broken HTML and updated to use Flask-Bootstrap and Flask-Nav

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_qrcode import QRcode
 
 from flask_nav import Nav
 from flask_nav.elements import *
+from wtforms import Label
 
 DATABASE_NAME = "QR-Users.db"
 
@@ -15,7 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.secret_key = os.urandom(24)
 app.secret_key = 'JIRN#@UNT#@(UTH@#(GN@#*'
-
 
 db = SQLAlchemy(app)
 QRcode(app)
@@ -26,7 +27,7 @@ login_manager.login_view = "login_page.login"
 
 nav = Nav()
 nav.init_app(app)
-
+Bootstrap(app)
 
 from blueprints.home import home_blueprint
 from blueprints.index import index_blueprint
@@ -48,10 +49,40 @@ app.register_blueprint(qr_code_blueprint)
 app.register_blueprint(locations_blueprint)
 app.register_blueprint(logout_blueprint)
 
-top_nav = Navbar(View('Widgits, Inc.', 'index'),
-                 View('Our Mission', 'home_page.home'),
-                 Link('Tech Support', 'http://techsupport.invalid/widgits_inc'))
-nav.register_element('top', top_nav)
+
+@nav.navigation()
+def anonymous_nav():
+    return Navbar(None,
+                  View('Home', 'index_page.index'),
+                  View('Login', 'login_page.login'),
+                  View('Register', 'register_page.register'))
+
+
+@nav.navigation()
+def admin_nav():
+    return Navbar(None,
+                  View('Home', 'index_page.index'),
+                  View('Account', 'home_page.home'),
+                  Subgroup('QR',
+                           View('QR Codes', 'qr_codes_page.qr_codes'),
+                           View('QR Generator', 'qr_generator_page.generate_qr_code'),
+                           View('QR Scanner', 'qr_code_scan_page.qr_scan')),
+                  Subgroup('Admin',
+                           View('Admin List', 'home_page.home')),
+                  View('Logout', 'logout_page.logout'))
+
+
+@nav.navigation()
+def user_nav():
+    return Navbar(None,
+                  View('Home', 'index_page.index'),
+                  View('Account', 'home_page.home'),
+                  View('Logout', 'logout_page.logout'))
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
